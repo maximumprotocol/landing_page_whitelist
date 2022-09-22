feab88c4f09b61a91f13b67850e4f41ddee2ec79
sheshiyer <sheshnarayan.iyer@gmail.com>
2022-09-22 12:08:54 +0530

    update _ partners and contact

--- form.php
+++ form.php
@@ -7,7 +7,7 @@
 	$message = isset( $_POST['feedbackMessage'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['feedbackMessage'] ) : "";
 
 	//Headers
-	$to = "team@maximumprotocol.finance";
+	$to = "contact@m3p.io";
     $subject = 'Contact Us';
 	$headers = "MIME-Version: 1.0\r\n";
 	$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
